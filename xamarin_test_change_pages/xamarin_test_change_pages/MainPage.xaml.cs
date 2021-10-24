905515c8747a3529175d7dc5988798bc33396ae4
John Doe <benicio.vanzwijndregt@gmail.com>
2021-10-25 12:18:40 +1300

    Add project files.

---
+++ xamarin_test_change_pages/xamarin_test_change_pages/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_test_change_pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void menuButton_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            switch(button.Text)
+            {
+                case "Menu":
+                    Navigation.PushAsync(new MainPage()); //move to Menu page
+                    break;
+                case "About":
+                    Navigation.PushAsync(new aboutPage()); //move to About page
+                    break;
+                case "Map":
+                    Navigation.PushAsync(new mapPage()); //move to Map page
+                    break;
+            }
+        }
+    }
+}
---
+++ xamarin_test_change_pages/xamarin_test_change_pages/aboutPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarin_test_change_pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class aboutPage : ContentPage
+    {
+        public aboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void menuButton_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            switch (button.Text)
+            {
+                case "Menu":
+                    Navigation.PushAsync(new MainPage()); //move to Menu page
+                    break;
+                case "About":
+                    Navigation.PushAsync(new aboutPage()); //move to About page
+                    break;
+                case "Map":
+                    Navigation.PushAsync(new mapPage()); //move to Map page
+                    break;
+            }
+        }
+    }
+}
